dbec228d3676b8870a8f7e218f32c09b628e6a6c
Andrei <zenin.an@do-student.ru>
2023-05-04 15:55:32 +0300

    continuing progress

--- counter_app/index.js
+++ counter_app/index.js
@@ -6,3 +6,11 @@ function increment(){
     count++
     countEl.innerText = count
 }
+// DOM = document object model (how u use js to modify a website)
+function save(){
+    // document.getElementById("save-el").innerText += ' ' + count + ' - ' 
+    document.getElementById("save-el").textContent += count + ' - '
+    console.log(count)
+    document.getElementById("count-el").textContent = 0
+    count = 0
+}
