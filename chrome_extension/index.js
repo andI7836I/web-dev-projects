6e34bede88d587ee0acd51cacbeb9fb73773d4ee
Andrei <zenin.an@do-student.ru>
2023-05-10 18:48:43 +0300

    continuing progress

--- chrome_extension/index.js
+++ chrome_extension/index.js
@@ -1,12 +1,18 @@
-let myLeads = ["a;slkdfja;lsdkfja;slkd", "s;adlfkjas;dl"]
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+let listItems = ""
 
-inputBtn.addEventListener("click", function(){
+inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
 })
 
 for (i in myLeads){
-    ulEl.textContent = myLeads[i]
+    ulEl.innerHTML +=
+    listItems += "<li>" + myLeads[i] + "</li>"
+    // const li = document.createElement("li")
+    // li.textContent = myLeads[i]
+    // ulEl.append(li)
 }
+ulEl.innerHTML = listItems
