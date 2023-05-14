0ae423579400cb0840a710250138adfdf19fa505
Andrei <zenin.an@do-student.ru>
2023-05-14 14:27:30 +0300

    continuing progress

--- shopping_cart/index.js
+++ shopping_cart/index.js
@@ -1,5 +1,11 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+const appSettings = {
+    databaseURL: ""
+}
 const addBtn = document.getElementById("add-button")
 const inputEl = document.getElementById("input-field")
 addBtn.addEventListener("click", function(){
-    console.log(inputEl.value)
+    let inputValue = inputEl.value
+    console.log(inputValue)
 })
