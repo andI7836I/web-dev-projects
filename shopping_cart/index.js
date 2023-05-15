2e22ca9235d5d5cc73116fd2990242a1df46c1be
Andrei <zenin.an@do-student.ru>
2023-05-15 07:34:52 +0300

    continuing progress

--- shopping_cart/index.js
+++ shopping_cart/index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://playground-6c11d-default-rtdb.firebaseio.com/"
@@ -15,13 +15,25 @@ const shoppingListEl = document.getElementById("shopping-list")
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     push(shoppingListInDB, inputValue)
-    clearInput("input-field")
-    appendItemTo(inputValue)
+    clearInput()
 })
-function clearInput(valuee){
-    document.getElementById(valuee).value = ""
+
+onValue(shoppingListInDB, function(snapshot){
+    let itemsArray = Object.values(snapshot.val())
+    clearShoppinglistEl()
+    for (let i in itemsArray){
+        let itemArray = itemsArray[i]
+        appendItemTo(itemArray)
+    }
+})
+
+function clearInput() {
+    inputFieldEl.value = ""
 }
 
 function appendItemTo(itemValue){
     shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+}
+function clearShoppinglistEl(){
+    shoppingListEl.innerHTML = ""
 }
